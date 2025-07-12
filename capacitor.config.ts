473b39b42626f350fcb381b7645d9fe14238006e
Lucasmasclee <lucas.rws@hotmail.com>
2025-07-12 23:08:11 +0200

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rijflow.app',
+  appId: 'com.mascelli.rijflow',
   appName: 'RijFlow',
   webDir: '.next',
   server: {
