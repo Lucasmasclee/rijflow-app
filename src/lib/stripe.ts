c68935de0b594580a50251f52ec0b22429677ad3
Lucasmasclee <lucas.rws@hotmail.com>
2025-08-02 12:04:03 +0200

    build error fix

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -4,7 +4,7 @@ import { Subscription } from '@/types/database'
 
 // Server-side Stripe instance
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-07-30.basil',
+  apiVersion: '2024-06-20',
 })
 
 // Client-side Stripe instance
