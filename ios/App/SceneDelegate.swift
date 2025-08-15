003afa7edebd95c0699d730929e476704da3eb78
Lucasmasclee <lucas.rws@hotmail.com>
2025-08-15 17:50:40 +0200

    IOS GLITCH GEFIXT

---
+++ ios/App/SceneDelegate.swift
@@ -0,0 +1,38 @@
+import UIKit
+import Capacitor
+
+@available(iOS 13.0, *)
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Fix voor FBSSceneSnapshotErrorDomain code 4
+        window = UIWindow(windowScene: windowScene)
+        let viewController = CAPBridgeViewController()
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+    }
+}
